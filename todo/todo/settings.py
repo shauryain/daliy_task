5867cd1477b59903941a0e4d4aaca0b160044494
shauryain <147681669+shauryain@users.noreply.github.com>
2024-09-22 21:23:54 +0530

    done
    done

--- todo/todo/settings.py
+++ todo/todo/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-7+g^io+^ghj=%e@ar)r+bhl7#v)nk28$d06$b$$@dr$78g@^or
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 
